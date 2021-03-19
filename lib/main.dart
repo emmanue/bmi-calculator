6f41c4ea9921b88f05609cdd3e87226720f45784
EMMANUEL FOTSO KAMGUE <brinkcorp@gmail.com>
2021-03-19 20:56:04 +0000

    another one

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+        accentColor: Colors.purple,
+      ),
       home: InputPage(),
     );
   }
