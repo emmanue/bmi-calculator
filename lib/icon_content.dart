991aec6c4b3ba5452848f478c923d974412a84d1
EMMANUEL FOTSO KAMGUE <brinkcorp@gmail.com>
2021-03-27 17:47:07 +0000

    added some increments

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// SEparation of concern to improve readability of code
+
 const labelTextStyle = TextStyle(
   fontSize: 18.0,
   color: Color(0xFF8D8E98),
