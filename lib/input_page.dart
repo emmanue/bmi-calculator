c8037f46c64daaf2cbef6ff1d936676a83899839
EMMANUEL FOTSO KAMGUE <brinkcorp@gmail.com>
2021-03-29 19:48:59 +0000

    solved error with text baseline

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -85,6 +85,7 @@ class _InputPageState extends State<InputPage> {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.alphabetic,
                           children: [
                             Text(
                               '180',
